ca550e7dd17fb1eb675f3870e2b9160459d41e46
André Soares Marques <79519540+AndreSMarques@users.noreply.github.com>
2026-09-25 16:27:43 +0000

    Modeificador de acesso private, get e set

--- java-virado-no-jiraya/ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ java-virado-no-jiraya/ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private  String nome;
+    private int idade;
+    private double[] salarios;
+    private double media = 0;
 
     public void impimirDados() {
         System.out.println("Nome " + this.nome);
@@ -17,7 +18,7 @@ public class Funcionario {
     }
 
     public void mediaSalarios() {
-        double media = 0;
+        
         if (salarios != null) {
             for (double i : salarios) {
                 media += i;
@@ -25,4 +26,33 @@ public class Funcionario {
             System.out.println("\nmedia: " + media / salarios.length);
         }
     }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
 }
--- java-virado-no-jiraya/ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ java-virado-no-jiraya/ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -6,9 +6,9 @@ public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
 
-        funcionario.nome = "Cabron";
-        funcionario.idade = 32;
-        funcionario.salarios = new double[] { 1300, 2000, 6000 };
+        funcionario.setNome("Cabron");
+        funcionario.setIdade(32); 
+        funcionario.setSalarios(new double[] { 1300, 2000, 6000 });
 
         funcionario.impimirDados();
     }
